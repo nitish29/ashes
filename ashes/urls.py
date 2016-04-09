e2f2c8fb879121bddf67722fedaff4e321a417eb
Rachna Shivangi <rachnash@buffalo.edu>
2016-04-09 15:53:13 -0400

    comparison tool basic flow working

--- ashes/urls.py
+++ ashes/urls.py
@@ -20,4 +20,5 @@ from ashes import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^playerCompareAction$', views.playerCompareAction, name='playerCompareAction'),
 ]
--- ashes/views.py
+++ ashes/views.py
@@ -4,7 +4,7 @@ from urllib.request import Request, urlopen
 from django.shortcuts import render
 from django.conf import settings
 import pdb
-from .models import PlayerStats
+from .models import PlayerStats, UserPlayers
 
 
 def home(request):
@@ -13,36 +13,50 @@ def home(request):
 		#pdb.set_trace()
 
 		players = PlayerStats.objects.all()
+		userPlayers = UserPlayers.objects.all()
 		# print(players)
 
-		for player in players:
-			print(player.player_name)
+		# for player in players:
+		# 	print(player.player_name)
 
+		# for myplayer in userPlayers:
+		# 	print(myplayer.player_name.player_name)
 
-
-		#data_fetched_from_api = fetchEventbriteCategories()
-
-	# 	if data_fetched_from_api['status'] == "success":
-
-	# 		categories = data_fetched_from_api['data']["categories"]	
-
-	# 	else:
-			
-	# 		errors.append(data_fetched_from_api['errors'])
-	# 		raise Exception(errors)
-
-	# 	if not errors:
-			
-	# 		context = { 'categories' : categories }
-			
-	# 	else:
-			
-	# 		errors.append('Error fetching API details')
-	# 		raise Exception(errors)
+		context = {'allPlayerList': players, 'myPlayerList': userPlayers}
 
 	except:
 		
 		errors.append('Error Completing request')
-	# 	context = { 'errors' : errors }
+		context = {'errors': errors}
 
-	return render(request, "playercompare.html")
+	return render(request, "playercompare.html", context)
+
+
+def playerCompareAction(request):
+	try:
+		errors = []
+		if request.GET['myPlayerSelect']:
+			my_player = UserPlayers.objects.get(player_name=request.GET['myPlayerSelect'])
+		if request.GET['allPlayerSelect']:
+			other_player = PlayerStats.objects.get(player_name=request.GET['allPlayerSelect'])
+
+		my_player_caa = my_player.player_name.caa
+		other_player_caa = other_player.caa
+
+		if my_player_caa > other_player_caa:
+			message = 'We recommend ' + my_player.player_name.player_name + ' over ' + other_player.player_name
+			print(message)
+		else:
+			message = 'We recommend ' + other_player.player_name + ' over ' + my_player.player_name.player_name
+			print(message)
+
+		players = PlayerStats.objects.all()
+		userPlayers = UserPlayers.objects.all()
+		
+		context = {'allPlayerList': players, 'myPlayerList': userPlayers, 'myPlayer': my_player, 'otherPlayer': other_player, 'message': message}
+
+	except:
+		errors.append('Error Completing request')
+		context = {'errors': errors}
+
+	return render(request, "playercompare.html", context)
