730983f98c2cd7ebb1af200a3abf5ab1d1f996ad
Rachna Shivangi <rachnash@buffalo.edu>
2016-04-18 04:17:57 -0400

    Negated Query working for User Tweets

--- ashes/views.py
+++ ashes/views.py
@@ -165,7 +165,7 @@ def makeSolrCall(search_query, queryType, week):
 	elif queryType == "playerTweets":
 		#pdb.set_trace()
 		request_params = urllib.parse.urlencode(
-			{'q': '*'+search_query+'*', 'wt': 'json', 'indent': 'true', 'rows': 5, 'start': 0, 'defType': 'dismax', 'qf': 'keywords entity text', 'bq': 'date^20 retweets^10 favorites^5', 'sort': 'date desc,retweets desc,favorites desc','fq': date})
+			{'q': '*' + search_query + '*', 'wt': 'json', 'indent': 'true', 'rows': 5, 'start': 0, 'defType': 'dismax', 'qf': 'keywords entity text', 'bq': 'date^20 retweets^10 favorites^5', 'sort': 'date desc,retweets desc,favorites desc', 'fq': '-username:(ICC,ESPNcricinfo,cricbuzz,CricketNDTV) ' + date})
 		request_params = request_params.encode('utf-8')
 		req = urllib.request.urlopen(settings.SOLR_BASEURL_TWEET,
 									 request_params)
